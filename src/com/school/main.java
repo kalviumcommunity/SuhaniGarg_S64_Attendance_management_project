7940eb8c5460b0a4d980ad0489690625e19cf97e
Suhani1234-5 <suhani.garg.s64@kalvium.community>
2025-09-05 09:47:17 +0530

    first commit

---
+++ src/com/school/main.java
@@ -0,0 +1,7 @@
+// package com.school;
+
+// public class main {
+//     public static void main(String[] args) {
+//         System.out.println("Welcome to the Attendance System!");
+//     }
+// }
